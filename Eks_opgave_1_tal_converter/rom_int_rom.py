79f693c7d48b33a3bd7a1f436a0d9e158e2153ab
MrCowsen <58846830+MrCowsen@users.noreply.github.com>
2021-10-21 11:05:10 +0200

    Thor ericsen (#16)
    * rom_int_rom
    
    *int to roman converter
    *roman to int converter
    
    * yeet more done

--- Eks_opgave_1_tal_converter/rom_int_rom.py
+++ Eks_opgave_1_tal_converter/rom_int_rom.py
@@ -1,45 +1,56 @@
 
-def partial_int_rom(x):
-        roman_numerals = ["M" , "CM" , "D" , "CD" , "C" , "XC" , "L" , "XL" , "X" , "IX" , "V" ,"IV", "I" ]
+def format_number(number):
+    ready_number = '{:,}'.format(number)
+    return ready_number
+
+
+def par_int_rom(x):
+        rom_num = ["M" , "CM" , "D" , "CD" , "C" , "XC" , "L" , "XL" , "X" , "IX" , "V" ,"IV", "I" ]
         integers = [1000 , 900 , 500 , 400 , 100 , 90 , 50 , 40 , 10 , 9 , 5 , 4 , 1]
         i = 0
-        roman_string = ""
+        rom_str = ""
         
         while x > 0:
             while (x // integers[i]) > 0:
-                roman_string += roman_numerals[i]
+                rom_str += rom_num[i]
                 x -= integers[i]
             i+=1
         
-        return roman_string
+        return rom_str
 
 
-def integer_to_roman(x):
+def int_to_rom(x):
     if x >= 4000:
         big = x // 1000
         
-        big_roman = partial_int_rom(big)
+        big_roman = par_int_rom(big)
         
         tiny = x  - big * 1000
         
-        tiny_roman = partial_int_rom(tiny)
+        tiny_roman = par_int_rom(tiny)
         
         
         print('_'*len(big_roman))
         print(f"{big_roman}{tiny_roman}")
+        
+    else:
+        print(par_int_rom(x))
     
 
-integer_to_roman(3888888)
+int_to_rom(3888888)
+int_to_rom(3888)
 
 
-def partial_rom_int(x):
-    roman_numerals = ["M" , "CM" , "D" , "CD" , "C" , "XC" , "L" , "XL" , "X" , "IX" , "V" ,"IV", "I" ]
+def par_rom_int(x):
+    rom_num = ["M" , "CM" , "D" , "CD" , "C" , "XC" , "L" , "XL" , "X" , "IX" , "V" ,"IV", "I" ]
     integers = [1000 , 900 , 500 , 400 , 100 , 90 , 50 , 40 , 10 , 9 , 5 , 4 , 1]
     value = 0
-    for i in range(len(roman_numerals)):
-        while x[:len(roman_numerals[i])] ==  roman_numerals[i]:
+    
+    for i in range(len(rom_num)):
+        while x[:len(rom_num[i])] ==  rom_num[i]:
             value += integers[i]
-            x = x[len(roman_numerals[i]):]
+            x = x[len(rom_num[i]):]
+
     return value
 
 
@@ -47,14 +58,19 @@ def roman_to_int(x):
     if ' ' in x:
         a,b = [str(i) for i in x.split(' ')]
         
-        big_int = partial_rom_int(a)
-        tiny_int = partial_rom_int(b)
+        big_int = par_rom_int(a)
+        tiny_int = par_rom_int(b)
         
-        print((big_int * 1000) + tiny_int)
+        number = (big_int * 1000) + tiny_int
+        
+        print(format_number(number))
     
     else:
-        print(partial_rom_int(x))    
+        number = par_rom_int(x)
+        print(format_number(number))
 
 g = 'MMMDCCCLXXXVIII DCCCLXXXVIII'
+q = 'MMMDCCCLXXXVIII'
 
 roman_to_int(g)
+roman_to_int(q)
