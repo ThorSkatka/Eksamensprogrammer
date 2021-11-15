4683c4f9ba9e187880e6a9001c2ccf87da7860cc
Mads <70133908+MrKahr@users.noreply.github.com>
2021-11-15 16:10:45 +0100

    Add files via upload

---
+++ Eks_opgave_3_kortspil/Deck.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 15 14:04:32 2021
+@author: mdsan
+"""
+import random
+
+class Kort:
+    '''Definer et kort'''
+
+cards1 = [1,2,3,4,5]
+cards2 = [1,2,3,4,5]
+
+class Deck:
+    '''Definer et stak kort'''
+    
+    # Make dummy deck with stack of cards 
+    def __init__(self, size = 4): 
+        self.size = 4
+        
+    # Shuffle a stack of cards
+    def cshuffle(self): 
+        random.shuffle(self)
+        return self 
+
+    # Play and remove top card from deck
+    def cplay(self): 
+        return self.pop(0)  
+    
+    # Add cards won to deck
+    def __add__(self, other):
+        return self + other
+
+# q = Stack.cshuffle(cards1)
+# p = Stack.cplay(cards1)
+z = Stack.__add__(cards1, cards2)
+
+
