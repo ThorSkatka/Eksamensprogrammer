8669d40190391f567661663979cf0b456e1cbfdd
MrCowsen <58846830+MrCowsen@users.noreply.github.com>
2021-11-15 14:19:11 +0100

    Skab eksamensopgave 3

---
+++ Eks_opgave_3_kortspil/Krig.py
@@ -0,0 +1,13 @@
+import random
+
+class Kort:
+    '''Definer et kort'''
+
+class Stak:
+    '''Definer et stak kort'''
+
+class Spiller:
+    '''Definer en spiller'''
+
+class Spillet:
+    '''Definer spillet'''
